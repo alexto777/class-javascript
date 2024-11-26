2df8ba66a0f45be021e7ea15d5a82f779608eace
alexcode <alexto777@gmail.com>
2024-11-25 20:48:58 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Mensaje por consola
+// console.log("Hola Mundo JS");
+
+let saludo = "Hola Mundo JS";
+
+let nombre = 'Alexander';
+
+console.log("Hola "+nombre);
+console.error("Error no muestra nombre completo");
+console.log("LA operación termina en la sumatoria total")
+
+let apellido = 'Castañeda';
+
+console.log(nombre+' '+apellido);
+
+let edad = "37";
+
+// let fechaCumpleanios = "07-06-1987"
+
+// Booleanos
+// True or false
+
+// let number edad = 30
+
+
+let numero = 5; // 5
+let numeroEnLetras = "5"; // "5"
+console.log(numero == numeroEnLetras);
+console.log(numero === numeroEnLetras);
+
+
+// alert("Esto es una alerta");
+// alert("Podemos envir cualquier mensaje");
+
+// alert("Hola "+nombre);
+
+
+// Debuggear
+// Encontrar errores, hacer pruebas de mi aplicación 
+
+
+let contador; //Inicializar mi variable
+
+// contador = 1; //Asignamos un valor
+
+let carrera = "Ingeniería de sistemas" //I + asi
+
+
