4740fcf57065444484755bdaa4d9fed4042c6f7d
alexcode <alexto777@gmail.com>
2024-11-27 20:08:55 -0500

    Funciones

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+// function saludar(nombre) {
+//     return "Hola "+nombre
+// }
+// console.log(saludar("Alex"));
+let saludo = "Que tengas un buen día!"
+
+function saludar(nombre) {
+    console.log(`Bienvendo ${nombre}! ${saludo}`)
+}
+saludar("Alex");
+
+function sumar (a,b) {
+    return a+b;
+}
+const resultado = sumar(3,5);
+
+function restar (a,b) {
+    return a-b;
+}
+const resultadoResta = restar(10,5)
+console.log(`El resultado de la resta es: ${resultadoResta}`)
+
+// Condicionales
+// function numeroPar (numero) {
+
+// }
